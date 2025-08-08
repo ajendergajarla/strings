04691c0f467c7afeae5d90f743724f3b3dada2fb
Ajender <ajendergajarla2@gmai.com>
2025-08-08 08:03:38 -0500

    Optimizing/2nd Approach

--- isomorphic_strings.py
+++ isomorphic_strings.py
@@ -33,3 +33,38 @@ also check whether the key is already present in dict if yes -> compare its valu
 also check whether the value is already present in dict if yes -> it has already mapped with another key that's y it is existed.
 if all checks done at the end -> means passed correctly return Yes
 '''
+
+""" 
+Optimizing:
+dictt.values() is taking O(n) in worst case and for loop O(n) so total -> TC: O(n^2)
+but here Space is: O(n) only.__name__
+ """
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        n = len(s)
+        dictt_s_t = {}
+        dictt_t_s = {}
+        
+        for i,j in zip(s, t):
+            if(i in dictt_s_t):
+                if(dictt_s_t[i] != j): return False
+
+            if(j in dictt_t_s):
+                if(dictt_t_s[j] != i): return False
+
+            dictt_s_t[i] = j
+            dictt_t_s[j] = i
+        return True
+            
+""" with Optimization Time reduced to O(N) But I think space will occupy O(2n) in worst case
+
+Ex: s = "abcdef" and t = "uvwxyz"
+
+dictt_s_t = { 'a': 'u','b': 'v','c': 'w','d': 'x','e': 'y','f': 'z' } -> O(N)
+                                                                                -> Space: O(2N)
+dictt_t_s = { 'u': 'a','v': 'b','w': 'c','x': 'd','y': 'e','z': 'f' } -> O(N)
+
+
+
+"""
